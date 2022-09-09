d217437b27878fb0e222d4bcc1212cacdbfc4aa4
bellukchips <bellukchips@gmail.com>
2022-09-09 14:41:24 +0700

    Part 2 :  Authentication Value Objects

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_ddd_learn/domain/core/failures.dart';
+import 'package:flutter_ddd_learn/domain/core/value_objects.dart';
+import 'package:flutter_ddd_learn/domain/core/value_validator.dart';
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+  // final Exception failure;
+  factory EmailAddress(String input) {
+    // ignore: unnecessary_null_comparison
+    assert(input != null);
+    return EmailAddress._(validateEmailAddress(input));
+  }
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+  // final Exception failure;
+  factory Password(String input) {
+    // ignore: unnecessary_null_comparison
+    assert(input != null);
+    return Password._(validatePassword(input));
+  }
+  const Password._(this.value);
+}
