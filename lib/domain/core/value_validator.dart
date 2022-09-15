581d3b8009a88cd2701eb53edf1d13220f8e893b
bellukchips <bellukchips@gmail.com>
2022-09-15 20:53:52 +0700

    Part 12 : Note Value Object

--- lib/domain/core/value_validator.dart
+++ lib/domain/core/value_validator.dart
@@ -1,5 +1,42 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_ddd_learn/domain/domain.dart';
+import 'package:kt_dart/kt.dart';
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+    String input, int maxLength) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(
+        ValueFailure.exceedingLength(failedValue: input, max: maxLength));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (input.contains('\n')) {
+    return left(ValueFailure.multiline(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+    KtList<T> input, int maxLength) {
+  if (input.size <= maxLength) {
+    return right(input);
+  } else {
+    return left(
+        ValueFailure.listToLong(failedValue: input, maxLength: maxLength));
+  }
+}
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const emailRegex =
@@ -13,8 +50,7 @@ Either<ValueFailure<String>, String> validateEmailAddress(String input) {
 }
 
 Either<ValueFailure<String>, String> validatePassword(String input) {
-  const passwordRegex =
-      r"""^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$""";
+  const passwordRegex = r"""^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$""";
   if (RegExp(passwordRegex).hasMatch(input)) {
     return right(input);
   } else {
